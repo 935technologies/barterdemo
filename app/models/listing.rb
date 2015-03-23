53318a7ed7faf33b1dd398a6ae7f2b27bef561e0
Ryan <ryan@935technologies.com>
2015-03-23 17:29:52 -0400

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,6 +10,9 @@ class Listing < ActiveRecord::Base
 	 validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
 	 validates :name, :description, :price, presence: true
-	 validates :price, numericality: { greater_than: 0}
+	 validates :price, numericality: {greater_than: 0}
 	 validates :image, :attachment_presence => true
+
+
+	 belongs_to :user 
 end
