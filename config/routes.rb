907910ed10f47a952d73af51bdbdb99624eaf776
Ryan <ryan@935technologies.com>
2015-03-25 20:19:59 -0400

    changed the ( to [

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :models
   resources :listings do 
-    resources :orders, only: (:new, :create)
+    resources :orders, only: [:new, :create]
   end
 
   get "page/about" 
